34f424f503c36ef1fc2ac807f6d1b726feba94ea
GangSan <ggomarain1302@gmail.com>
2023-08-25 23:18:46 +0900

    초기화

--- NovelConnect_NewSystem/Assets/01.Scripts/Scene/BaseScene.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseScene : MonoBehaviour
-{
-    public abstract void Init();
-    public abstract void Clear();
-}
