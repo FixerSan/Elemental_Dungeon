5bd6708241215ca8399d567e4c58235580bbf4ce
GangSan <ggomarain1302@gmail.com>
2023-10-05 20:13:22 +0900

    수정: Build_10_05

--- Novel_Connect/Assets/01.Scripts/Inventory & Item/Item/ItemController.cs
+++ Novel_Connect/Assets/01.Scripts/Inventory & Item/Item/ItemController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-using UnityEditor.UIElements;
+
 
 public class ItemController : MonoBehaviour
 {
--- Novel_Connect/Assets/01.Scripts/UI/UISlot/UISlot_Inventory.cs
+++ Novel_Connect/Assets/01.Scripts/UI/UISlot/UISlot_Inventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Rendering.LookDev;
+
 
 public class UISlot_Inventory : UISlot
 {
